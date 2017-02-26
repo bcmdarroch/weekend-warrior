961c44370c66ff53d0587b57dac592467a85c9fe
Brenna <bcmdarroch@gmail.com>
2017-02-26 14:14:45 -0800

    added Alouette, lines, verse, sing stubs

---
+++ 02-alouette/alouette.rb
@@ -0,0 +1,18 @@
+
+class Alouette
+
+  # Alouette#lines_for_verse will do the work of generating all those Et <part>! lines for a given verse. It should return an array of strings, without repeats.
+  # Note that verses are 0-indexed.
+  # array[0] "et la tete"
+  def lines_for_verse(verse_num)
+  end
+
+  # Alouette#verse will build the requested verse. It should return a string. For example, if you were to call verse(2), it would return
+  def verse
+  end
+
+  # Alouette#sing will build the entire song, formatted as in alouette_lyrics.txt. There should be a blank line between verses and refrains. The value returned should return a string.
+  def sing
+  end
+
+end
