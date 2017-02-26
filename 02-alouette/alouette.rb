505b81723bc6db41c70efc06f12af65b0c04278f
Brenna <bcmdarroch@gmail.com>
2017-02-26 15:32:01 -0800

    verse passes minitests

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -18,8 +18,30 @@ class Alouette
     return lines[0..verse_num].reverse
   end
 
-  # Alouette#verse will build the requested verse. It should return a string. For example, if you were to call verse(2), it would return
-  def self.verse
+#   # Alouette#verse will build the requested verse. It should return a string. For example, if you were to call verse(2), it would return
+#   Je te plumerai les yeux.
+# Je te plumerai les yeux.
+# Et les yeux!
+# Et les yeux!
+# Et le bec!
+# Et le bec!
+# Et la tête!
+# Et la tête!
+# Alouette!
+# Alouette!
+# A-a-a-ah
+
+  # refrain1 = "Alouette, gentille alouette,\nAlouette, je te plumerai."
+
+  def self.verse(verse_num)
+    lines1 = "Je te plumerai" + self.lines_for_verse(verse_num).reverse[verse_num][2..-2] + ".\n"
+    lines2 = ""
+    Alouette.lines_for_verse(verse_num).each do |line|
+       lines2 += (line + "\n") * 2
+     end
+    refrain2 = "Alouette!\nAlouette!\nA-a-a-ah"
+
+    (lines1 * 2) + lines2 + refrain2
   end
 
   # Alouette#sing will build the entire song, formatted as in alouette_lyrics.txt. There should be a blank line between verses and refrains. The value returned should return a string.
@@ -27,3 +49,7 @@ class Alouette
   end
 
 end
+
+puts "lines for verse: #{Alouette.lines_for_verse(2)}"
+puts "verses:\n#{Alouette.verse(2)}"
+# puts Alouette.sing
